a212034442cf6fca8860bbe596a64d75578f4936
Zach Eriksen <zeriksen@fnoex.com>
2020-11-25 13:39:16 -0600

    Initial Commit

---
+++ TV/TVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVApp.swift
+//  TV
+//
+//  Created by CRi on 11/25/20.
+//
+
+import SwiftUI
+
+@main
+struct TVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
